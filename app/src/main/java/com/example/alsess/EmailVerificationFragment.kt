84fb36647546f151ddef9a43413fc56be6ba8657
Ali Sağlık <alisaglik10@gmail.com>
2024-02-02 01:17:28 +0300

    Email verification fragment was designed and codes were created

--- app/src/main/java/com/example/alsess/EmailVerificationFragment.kt
+++ app/src/main/java/com/example/alsess/EmailVerificationFragment.kt
@@ -12,13 +12,64 @@ import com.google.firebase.auth.FirebaseAuth
 
 class EmailVerificationFragment : Fragment() {
     private lateinit var viewBinding: FragmentEmailVerificationBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         viewBinding = FragmentEmailVerificationBinding.inflate(inflater, container, false)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = firebaseAuth.currentUser!!
+
+        if (currentUser.isEmailVerified) {
+            viewBinding.fragmentEmailVerificationTxvCurrentEmail
+                .setCompoundDrawablesWithIntrinsicBounds(R.drawable.asset_tic, 0, 0, 0)
+        }
+
+        viewBinding.fragmentEmailVerificationTxvCurrentEmail.text = firebaseAuth.currentUser!!.email
+
+        viewBinding.fragmentEmailVerificationToolbar.setNavigationOnClickListener {
+            Navigation.findNavController(it).popBackStack()
+        }
+        viewBinding.fragmentEmailVerificationBtnEmailVerification.setOnClickListener {
+            firebaseEmailVerification()
+        }
         return viewBinding.root
+
+    }
+
+    //The user stops the app to look at the email and the data is refreshed when they come back
+    override fun onResume() {
+        super.onResume()
+        refreshFirebaseEmailVerification()
+    }
+
+    //For live refresh of data
+    fun refreshFirebaseEmailVerification() {
+        val currentUser = firebaseAuth.currentUser!!
+        currentUser.reload().addOnSuccessListener { void ->
+            if (currentUser.isEmailVerified) {
+                viewBinding.fragmentEmailVerificationTxvCurrentEmail
+                    .setCompoundDrawablesWithIntrinsicBounds(R.drawable.asset_tic, 0, 0, 0)
+            }
+        }
+    }
+
+    fun firebaseEmailVerification() {
+        val currentUser = firebaseAuth.currentUser!!
+        if (currentUser.isEmailVerified) {
+            Toast.makeText(context, getString(R.string.email_already_verified), Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            currentUser.sendEmailVerification()
+            Toast.makeText(
+                context,
+                getString(R.string.verification_link_sent),
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
 
     }
 }
